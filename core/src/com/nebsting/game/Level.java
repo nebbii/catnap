4c28d615c2f4eb0fcc65994e33ef01a9c263a61b
Ben <ben.wolthuis@gmail.com>
2019-12-04 23:51:15 +0100

    Added a base tileset for testing

--- core/src/com/nebsting/game/Level.java
+++ core/src/com/nebsting/game/Level.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -15,18 +18,28 @@ public class Level implements Screen {
 
     Viewport viewport;
     Camera camera;
+    TmxMapLoader maploader;
+    OrthogonalTiledMapRenderer renderer;
+    TiledMap map;
 
     public Level(final Catnap game) {
         this.game = game;
         camera = new PerspectiveCamera();
         viewport = new FitViewport(960, 720, camera);
+
+        //maploader = new TmxMapLoader();
+        //map = maploader.load("level/test.tmx");
+        //renderer = new OrthogonalTiledMapRenderer(map);
+
+        //camera.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0.3f, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        //renderer.render();
     }
 
     @Override
