c84b064093a2f1278f3fd1027e8c28e3bfd43465
Ben <ben.wolthuis@gmail.com>
2019-12-09 10:31:56 +0100

    Moved over most of LevelOne's logic to Level, eventually to be moved to an external file

--- core/src/com/nebsting/game/Level.java
+++ core/src/com/nebsting/game/Level.java
@@ -4,6 +4,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class Level implements Screen {
@@ -13,11 +17,23 @@ public class Level implements Screen {
     Viewport viewport;
     OrthographicCamera camera;
 
-    public Level(final Catnap game) {
+    Toriyasu toriyasu;
+
+    TiledMap tiledMap;
+    TiledMapRenderer tiledMapRenderer;
+
+    public Level(final Catnap game, String map, float playerX, float playerY) {
         this.game = game;
 
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
+
+        toriyasu = new Toriyasu();
+        toriyasu.x = 300;
+        toriyasu.y = 200;
+
+        tiledMap = new TmxMapLoader().load("level/" + map + ".tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, w, h);
@@ -29,11 +45,25 @@ public class Level implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+
+        toriyasu.logic(delta);
+
+        camera.position.set(toriyasu.x + (toriyasu.getWidth() / 2), 350, 0);
         camera.update();
+
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.batch.draw(toriyasu.sprite, toriyasu.x, toriyasu.y);
+        game.batch.end();
     }
 
     @Override
     public void dispose() {
+        game.batch.dispose();
+        tiledMap.dispose();
     }
 
     @Override
--- core/src/com/nebsting/game/LevelOne.java
+++ core/src/com/nebsting/game/LevelOne.java
@@ -1,49 +1,13 @@
 package com.nebsting.game;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class LevelOne extends Level {
-
-    Toriyasu toriyasu;
-
-    TiledMap tiledMap;
-    TiledMapRenderer tiledMapRenderer;
-
     public LevelOne(final Catnap game) {
-        super(game);
-
-        toriyasu = new Toriyasu();
-        toriyasu.x = 300;
-        toriyasu.y = 200;
-
-        tiledMap = new TmxMapLoader().load("level/testLevel2.tmx");
-        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+        super(game, "testLevel2", 300f, 200f);
     }
 
     public void render(float delta) {
         super.render(delta);
-
-        tiledMapRenderer.setView(camera);
-        tiledMapRenderer.render();
-
-        toriyasu.logic(delta);
-
-        camera.position.set(toriyasu.x + (toriyasu.getWidth() / 2), 350, 0);
-
-        game.batch.begin();
-        game.batch.setProjectionMatrix(camera.combined);
-        game.batch.draw(toriyasu.sprite, toriyasu.x, toriyasu.y);
-        game.batch.end();
-    }
-
-    public void dispose() {
-        game.batch.dispose();
-        tiledMap.dispose();
     }
 }
 
